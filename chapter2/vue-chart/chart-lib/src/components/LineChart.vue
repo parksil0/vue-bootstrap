34014fa09d74e62c44f6d1142ec6e37ed0d18f70
parksil <parksil00@naver.com>
2021-02-15 00:47:47 +0900

    .

---
+++ chapter2/vue-chart/chart-lib/src/components/LineChart.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+      <canvas ref="lineChart" id="lineChart"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        // var ctx = document.getElementById('lineChart').getContext('2d');
+        // var ctx = this.$refs.lineChart.getContext('2d');
+
+        // eslint-disable-next-line no-unused-vars
+        var chart = new this.$_Chart(this.$refs.lineChart.getContext('2d'), {
+            // The type of chart we want to create
+            type: 'line',
+
+            // The data for our dataset
+            data: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [{
+                    label: 'My First dataset',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: [0, 10, 5, 2, 20, 30, 45]
+                }]
+            },
+
+            // Configuration options go here
+            options: {}
+        });
+    }
+}
+</script>
+
+<style>
+
+</style>
